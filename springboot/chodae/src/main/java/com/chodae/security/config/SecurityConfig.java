a36fa38462638535a7567a984ed6a1e30f251283
JongKuk Seo <jongkuk.dev@gmail.com>
2022-03-15 19:39:55 +0900

    오류수정

--- springboot/chodae/src/main/java/com/chodae/security/config/SecurityConfig.java
+++ springboot/chodae/src/main/java/com/chodae/security/config/SecurityConfig.java
@@ -5,6 +5,8 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
@@ -12,7 +14,7 @@ import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import lombok.extern.java.Log;
 
 @Log
-@Configuration @EnableWebSecurity
+@Configuration @EnableWebSecurity 
 public class SecurityConfig extends WebSecurityConfigurerAdapter{
 	
 	@Override
@@ -41,6 +43,11 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
         source.registerCorsConfiguration("/**", configuration);
         return source;
     }
+    
+  @Bean
+  public PasswordEncoder passwordEncoder() {
+  	return new BCryptPasswordEncoder();
+  }
 	
 	
 }
